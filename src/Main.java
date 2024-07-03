8c8003da920f7c1a9f7795ed4fe36479ce56e496
sehabzks <99829809+sehabzks@users.noreply.github.com>
2024-07-03 12:26:37 +0300

    asdasd

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello wosdasdasdasddasdasdasdasdrld!");
+        System.out.println("Hello wosdasdasasdasdddddddddddddasddasdasdasdasdrld!");
     }
 }
